5e0a9c54643ff01afe40c531a4c19144397fb012
danielttttttt <danikingneg@gmail.com>
2022-10-13 12:51:34 -0700

    hello

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>                                                                                                                      #include <unistd.h>
+#include <stdio.h>                                                                                                                      
 
 /**
  * main -Entry point
